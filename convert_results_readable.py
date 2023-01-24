706950d3b6b23578f4a64678dcb2b86bfe67f825
Bram Vanroy <Bram.Vanroy@UGent.be>
2023-01-24 15:52:18 +0100

    make more robust

--- convert_results_readable.py
+++ convert_results_readable.py
@@ -5,8 +5,8 @@ from typing import Optional
 
 
 def convert_size(size_bytes: int):
-    if size_bytes == 0:
-        return "0 B"
+    if size_bytes <= 0:
+        return f"{size_bytes} B"
     size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
     i = int(math.floor(math.log(size_bytes, 1024)))
     p = math.pow(1024, i)
